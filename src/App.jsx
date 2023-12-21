61622fd2006973b75f709758260500947e0c06ca
du <tb.poopp@gmail.com>
2023-12-21 20:42:56 +0700

    feat: add exercise state 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingInputText, setGreetingInputText] =
+    useState("Greeting Message");
+  const changeGreetingMsg = (newGreeting) => {
+    setGreetingInputText(newGreeting);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingInputText}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button
+          onClick={() => {
+            changeGreetingMsg("สวัสดี!");
+          }}
+        >
+          สวัสดี!
+        </button>
+        <button
+          onClick={() => {
+            changeGreetingMsg("Hi!");
+          }}
+        >
+          Hi!
+        </button>
+        <button
+          onClick={() => {
+            changeGreetingMsg("你好!");
+          }}
+        >
+          你好!
+        </button>
       </div>
     </div>
   );
