81329d9c016609aba8d55c10b601e3b9598b8198
nattawat <frank.nattawat@hotmail.com>
2023-12-21 14:38:07 +0700

    feat:complete ex

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+
+  const handleAddThai = () => {
+    setGreeting("สวัสดี!");
+  };
+  const handleAddEng = () => {
+    setGreeting("Hi!");
+  };
+  const handleAddChinese = () => {
+    setGreeting("你好!");
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={handleAddThai}>สวัสดี!</button>
+        <button onClick={handleAddEng}>Hi!</button>
+        <button onClick={handleAddChinese}>你好!</button>
       </div>
     </div>
   );
